60517b9737fcb94e50b343396952fb24dd121092
EgorShirokovFIIT1 <egorshir018@yandex.ru>
2022-10-27 15:15:58 +0300

    not equal matrix assign method and test don't work(problems with memory)

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -4,12 +4,13 @@
 void main()
 {
   TDynamicMatrix<int> a(2), b(2), c(2);
-  TDynamicVector<int> v(2),v1(2);
+  TDynamicVector<int> v(2),v1(3);
   int i, j;
   v[0] = 1;
   v[1] = 2;
   v1[0] = 2;
   v1[1] = 3;
+  v1[2] = 4;
   cout << "Testing matrix:"<< endl;
   for (i = 0; i < 2; i++)
       for (j = 0; j < 2; j++)
@@ -20,13 +21,15 @@ void main()
   cout << "Matrix a: " << endl << a << endl;
   cout << "Matrix b: " << endl << b << endl;
   cout << "Matrix c:" << endl << c << endl;
-  c = a;
-  cout << "Matrix c=a:" << endl << c << endl;
-  cout << "Matrix a*b:" << endl << (a*b) << endl;
-  cout << "Matrix a-b:" << endl << (b - a) << endl;
-  cout << "Matrix a+b:" << endl << (a+b) << endl;
+  cout << "Matrix a * b:" << endl << (a * b) << endl;
+  cout << "Matrix a - b:" << endl << (b - a) << endl;
+  cout << "Matrix a + b:" << endl << (a+b) << endl;
   cout << "Vector v:" << endl << v << endl;
   cout << "Vector v1:" << endl << v1 << endl;
-  cout << "Vector v1:" << endl << v*v1 << endl;
-  cout << "Multip a*v:" << endl << a*v << endl;
+  //cout << "Vector v*v1:" << endl << v*v1 << endl;
+  //cout << "Multip a*v:" << endl << a*v << endl;
+  v = v1;
+  cout << "v = v1" << endl << v << endl;
+  c = a;
+  cout << "Matrix c = a:" << endl << c << endl;
 }
--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -57,66 +57,103 @@ TEST(TDynamicMatrix, can_set_and_get_element)
 
 TEST(TDynamicMatrix, throws_when_set_element_with_negative_index)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2);
+	ASSERT_ANY_THROW(m[-1][0] = 10);
+	ASSERT_ANY_THROW(m[0][-1] = 10);
 }
 
 TEST(TDynamicMatrix, throws_when_set_element_with_too_large_index)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2);
+	ASSERT_ANY_THROW(m[2][0] = 10);
+	ASSERT_ANY_THROW(m[0][2] = 10);
 }
 
 TEST(TDynamicMatrix, can_assign_matrix_to_itself)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2);
+	ASSERT_NO_THROW(m = m);
 }
 
-TEST(TDynamicMatrix, can_assign_matrices_of_equal_size)
+TEST(TDynamicMatrix, can_assign_matrixes_of_equal_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2),m1(2);
+	ASSERT_NO_THROW(m = m1);
 }
 
 TEST(TDynamicMatrix, assign_operator_change_matrix_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(3);
+	m[0][0] = 1;
+	m1[2][2] = 2;
+	int a = m.getSize();
+	m = m1;
+	int b = m.getSize();
+	EXPECT_NE(a, b);
 }
 
-TEST(TDynamicMatrix, can_assign_matrices_of_different_size)
+TEST(TDynamicMatrix, cant_assign_matrices_of_different_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(3);
+	ASSERT_ANY_THROW(m + m1);
 }
 
 TEST(TDynamicMatrix, compare_equal_matrices_return_true)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(2);
+	m[0][0] = 10;
+	m1 = m;
+	EXPECT_TRUE(m == m1);
 }
 
 TEST(TDynamicMatrix, compare_matrix_with_itself_return_true)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2);
+	m[0][0] = 10;
+	EXPECT_TRUE(m == m);
 }
 
 TEST(TDynamicMatrix, matrices_with_different_size_are_not_equal)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(3);
+	EXPECT_FALSE(m == m1);
 }
 
 TEST(TDynamicMatrix, can_add_matrices_with_equal_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(2), m2(2);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			m[i][j] = 1;
+			m1[i][j] = 2;
+			m2[i][j] = 3;
+		}
+	}
+	EXPECT_EQ(m2, m + m1);
 }
 
 TEST(TDynamicMatrix, cant_add_matrices_with_not_equal_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(3);
+	ASSERT_ANY_THROW(m + m1);
 }
 
 TEST(TDynamicMatrix, can_subtract_matrices_with_equal_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(2), m2(2);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			m[i][j] = 3;
+			m1[i][j] = 2;
+			m2[i][j] = 1;
+		}
+	}
+	EXPECT_EQ(m2, m - m1);
 }
 
 TEST(TDynamicMatrix, cant_subtract_matrixes_with_not_equal_size)
 {
-	ADD_FAILURE();
+	TDynamicMatrix<int> m(2), m1(3);
+	ASSERT_ANY_THROW(m - m1);
 }
 
